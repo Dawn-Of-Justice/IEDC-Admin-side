40294fab6552709ead9ee4074d2242cfdbafa3c2
Dawn of Justize <salosoja@gmail.com>
2022-06-09 19:21:13 +0530

    Revert "Added Js functions"
    This reverts commit 9337d17bf693aee394544b32b5dfe10308c8296e.

--- static/js/siteAnalyticsNumber.js
+++ static/js/siteAnalyticsNumber.js
@@ -16,52 +16,9 @@
 //     }
 //     updateCount1();
 // });
-// $(document).ready(function(){
-//     $(".number-numberdata").counterUp({
-//         delay:10,
-//         time:1200
-//     });
-// });
-
-
-
-
-
-//  Changes what I made down below
-
-// var runAlready;
-// var counting;
-
-// function startCounter(a) {
-//   runAlready = true;
-//   counting = true;
-//   $('.number-numberdata').each(function() {
-//     var $this = $(this);
-//     $this.text(a);
-//     countTo = $this.attr('data-count');
-//     $({
-//       countNum: $this.text()
-//     }).animate({
-//       countNum: countTo
-//     }, 
-//     {
-//       duration: 2000,
-//       easing: 'swing',
-//       step: function() {
-//         $this.text(Math.floor(this.countNum));
-//       },
-//       complete: function() {
-//         $this.text(this.countNum);
-//         counting = false;
-//       }
-//     });
-//   })
-// }
-
-// $(window).scroll(function() {
-//   var oTop = $('#counter').offset().top;
-//   var a = 0;
-//   if (!runAlready && $(window).scrollTop() > oTop || !counting && $(window).scrollTop() < oTop) {
-//     startCounter(a);
-//   }
-// });
+$(document).ready(function(){
+    $(".number-numberdata").counterUp({
+        delay:10,
+        time:1200
+    });
+});
